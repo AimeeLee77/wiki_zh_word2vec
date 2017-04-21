49ab26e12de0d4e1e22adecf5dae5c169ebfa837
AimeeLee77 <lizz_8777@163.com>
2017-04-21 15:46:44 +0800

    add

---
+++ 2_jieba_participle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+#逐行读取文件数据进行jieba分词
+
+import jieba
+import jieba.analyse
+import jieba.posseg as pseg #引入词性标注接口 
+import codecs,sys
+
+
+if __name__ == '__main__':
+    f = codecs.open('wiki.zh.simp.txt', 'r', encoding='utf8')
+    target = codecs.open('wiki.zh.simp.seg.txt', 'w', encoding='utf8')
+    print 'open files.'
+
+    lineNum = 1
+    line = f.readline()
+    while line:
+        print '---processing ',lineNum,' article---'
+        seg_list = jieba.cut(line,cut_all=False)
+        line_seg = ' '.join(seg_list)
+        target.writelines(line_seg)
+        lineNum = lineNum + 1
+        line = f.readline()
+
+    print 'well done.'
+    f.close()
+    target.close()
---
+++ 3_train_word2vec_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8  -*-
+#使用gensim word2vec训练脚本获取词向量
+
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')# 忽略警告
+
+import logging
+import os.path
+import sys
+import multiprocessing
+
+from gensim.corpora import WikiCorpus
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+
+
+if __name__ == '__main__':
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+
+    logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+
+    # inp为输入语料, outp1 为输出模型, outp2为原始c版本word2vec的vector格式的模型
+    inp = 'wiki.zh.simp.seg.txt'
+    outp1 = 'wiki.zh.text.model'
+    outp2 = 'wiki.zh.text.vector'
+
+    # 训练skip-gram模型
+    model = Word2Vec(LineSentence(inp), size=400, window=5, min_count=5,
+                     workers=2)
+
+    # 保存模型
+    model.save(outp1)
+    model.save_word2vec_format(outp2, binary=False)
+
+
